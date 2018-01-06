28543b622e053584e8a2f10f46b473bb9ea99627
vd <vdbazinga79@gmail.com>
2018-01-06 11:47:42 +0530

    complete code

--- NavSwitch/ViewController.swift
+++ NavSwitch/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    @IBOutlet var scrollView: UIScrollView!
+    
+    
+    
+    @IBAction func upButton(_ sender: UIButton) {
+        
+       scrollView.scrollsToTop = true
+        
+        scrollView.scrollRectToVisible(label.frame, animated: true)
+        
+    }
+    
+    
+    
+    @IBOutlet var indicator: UIActivityIndicatorView!
+    
+    
+    @IBAction func `switch`(_ sender: UISwitch) {
+        
+        if sender.isOn == true {
+            
+            indicator.startAnimating()
+        }
+        else {
+            
+            indicator.stopAnimating()
+        }
+    }
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
